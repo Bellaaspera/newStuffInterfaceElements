3f64063b8f6a063e02022a24a90c67aa253546cc
Bellaaspera <lanasenatorova@gmail.com>
2022-06-24 15:38:11 +0300

    Lesson

--- newStuffInterfaceElements/ViewController.swift
+++ newStuffInterfaceElements/ViewController.swift
@@ -9,11 +9,94 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    MARK: IB Outlets
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var switchLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        setupSlider()
+        setupMainLabel()
     }
 
+//    MARK: IB Actions
+    @IBAction func segmentedControlAction() {
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .blue
+        default:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .yellow
+        }
+    }
 
+    @IBAction func sliderAction() {
+        mainLabel.text = "\(slider.value)"
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(slider.value))
+    }
+    
+    @IBAction func doneButtonPressed() {
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            showAlert(with: "Text field is empty", and: "Please enter your name!")
+            return
+        }
+        
+        if let _ = Double(inputText) {
+            showAlert(with: "Wrong format!", and: "Please enter your name!")
+            return
+        }
+        
+        mainLabel.text = textField.text
+    }
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        datePicker.isHidden = !sender.isOn
+        switchLabel.text = sender.isOn ? "Hide Date Picker" : "Show Date Picker"
+    }
+    
+    @IBAction func datePickerAction() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        mainLabel.text = dateFormatter.string(from: datePicker.date)
+    }
+    
+    
+    //    MARK: Private methods
+    private func setupMainLabel() {
+        mainLabel.text = String(slider.value)
+        mainLabel.font = UIFont.systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+    }
+    
+    private func setupSlider() {
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+    }
 }
 
+// MARK: UIAlertController
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.textField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
